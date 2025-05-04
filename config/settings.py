d09c38f494018c6cb1c7cf554b66f13b23a492bc
timtim-hub <user@example.com>
2025-05-04 17:27:03 +0200

    Cleanup: Only CROSSPOST_SUBREDDITS list remains, expanded with new subreddits (no comments or extra settings)

--- config/settings.py
+++ config/settings.py
@@ -7,14 +7,6 @@ This bot comments on posts in specified subreddits based on configured settings.
 MAX_DAILY_COMMENTS = 50  # Maximum number of comments per day
 COMMENT_INTERVAL_SECONDS = 600  # Minimum time between comments (10 minutes)
 
-# Post Eligibility
-MAX_POST_AGE_HOURS = 24  # Maximum age of posts to comment on
-MIN_POST_SCORE = 1  # Minimum score (upvotes) required for a post
-
-# Subreddit Settings
-TARGET_SUBREDDITS = ["subreddit1", "subreddit2"]  # List of subreddits to target
-SUBREDDIT_BLACKLIST = []  # Subreddits to avoid
-
 # Comment Templates
 COMMENT_TEMPLATES = [
     "Great post! I found this very interesting because {post_title}.",
@@ -57,91 +49,6 @@ LOG_LEVEL = "INFO"  # Options: DEBUG, INFO, WARNING, ERROR, CRITICAL
 
 # Business/Marketing/SEO Subreddits for Crossposting
 CROSSPOST_SUBREDDITS = [
-    "Entrepreneur",
-    "startupsavant",
-    "Leadership",
-    "StartupsHelpStartups",
-    "TheFounders",
-    "growmybusiness",
-    "venturecapital",
-    "startups",
-    "advancedentrepreneur",
-    "marketing",
-    "ladybusiness",
-    "productivity",
-    "socialmedia",
-    "askmarketing",
-    "digitalmarketing",
-    "webmarketing",
-    "SEO",
-    "PPC",
-    "contentmarketing",
-    "advertising",
-    "bigseo",
-    "ecommerce",
-    "shopify",
-    "affiliatemarketing",
-    "smallbusiness",
-    "business",
-    "consulting",
-    "branding",
-    "publicrelations",
-    "influencermarketing",
-    "dropship",
-    "dropshipping",
-    "amazonFBA",
-    "amazon",
-    "passive_income",
-    "hustle",
-    "bizop",
-    "b2b",
-    "b2c",
-    "marketresearch",
-    "productmanagement",
-    "saas",
-    "growthhacking",
-    "sidehustle",
-    "workonline",
-    "freelance",
-    "remotework",
-    "remotebusiness",
-    "remotestartups",
-    "remotemarketing",
-    "remotefreelance",
-    "remotefounders",
-    "remoteteams",
-    "remotemanagement",
-    "remotebizdev",
-    "remotebizops",
-    "remotebizowner",
-    "remotebizowners",
-    "remotebizgrowth",
-    "remotebizmarketing",
-    "remotebizsales",
-    "remotebizconsulting",
-    "remotebizstrategy",
-    "remotebizstartup",
-    "remotebizstartups",
-    "remotebizsaas",
-    "remotebizfounders",
-    "remotebizinvesting",
-    "remotebizfinance",
-    "remotebizaccounting",
-    "remotebizbookkeeping",
-    "remotebizconsultants",
-    "remotebizdevops",
-    "remotebizgrowthhacking",
-    "remotebizgrowthmarketing",
-    "remotebizgrowthsales",
-    "remotebizgrowthconsulting",
-    "remotebizgrowthstrategy",
-    "remotebizgrowthstartup",
-    "remotebizgrowthstartups",
-    "remotebizgrowthsaas",
-    "remotebizgrowthfounders",
-    "remotebizgrowthinvesting",
-    "remotebizgrowthfinance",
-    "remotebizgrowthaccounting",
-    "remotebizgrowthbookkeeping",
-    "remotebizgrowthconsultants"
+    "Entrepreneur", "startupsavant", "Leadership", "StartupsHelpStartups", "TheFounders", "growmybusiness", "venturecapital", "startups", "advancedentrepreneur", "marketing", "ladybusiness", "productivity", "socialmedia", "askmarketing", "digitalmarketing", "webmarketing", "SEO", "PPC", "contentmarketing", "advertising", "bigseo", "ecommerce", "shopify", "affiliatemarketing", "smallbusiness", "business", "consulting", "branding", "publicrelations", "influencermarketing", "dropship", "dropshipping", "amazonFBA", "amazon", "passive_income", "hustle", "bizop", "b2b", "b2c", "marketresearch", "productmanagement", "saas", "growthhacking", "sidehustle", "workonline", "freelance", "remotework", "remotebusiness", "remotestartups", "remotemarketing", "remotefreelance", "remotefounders", "remoteteams", "remotemanagement", "remotebizdev", "remotebizops", "remotebizowner", "remotebizowners", "remotebizgrowth", "remotebizmarketing", "remotebizsales", "remotebizconsulting", "remotebizstrategy", "remotebizstartup", "remotebizstartups", "remotebizsaas", "remotebizfounders", "remotebizinvesting", "remotebizfinance", "remotebizaccounting", "remotebizbookkeeping", "remotebizconsultants", "remotebizdevops", "remotebizgrowthhacking", "remotebizgrowthmarketing", "remotebizgrowthsales", "remotebizgrowthconsulting", "remotebizgrowthstrategy", "remotebizgrowthstartup", "remotebizgrowthstartups", "remotebizgrowthsaas", "remotebizgrowthfounders", "remotebizgrowthinvesting", "remotebizgrowthfinance", "remotebizgrowthaccounting", "remotebizgrowthbookkeeping", "remotebizgrowthconsultants",
+    "business", "entrepreneur", "smallbusiness", "startups", "startup", "businessideas", "consulting", "leanstartup", "sidehustle", "sideproject", "freelance", "growmybusiness", "bizdev", "sales", "productmanagement", "b2b", "founders", "bootstrapped", "leadership", "organizationdevelopment", "operations", "venturecapital", "sustainability", "financesmallbusiness", "businessIntelligence", "femaleentrepreneurs", "MBAs", "consultingcareers", "supplychain", "careerquestionsbiz", "marketing", "marketingprofs", "askmarketing", "marketingadvice", "marketingstrategy", "marketingautomation", "CMO", "marketingops", "growthhackingideas", "digital_marketing", "coldemailing", "emailmarketing", "marketingresearch", "abtesting", "saasmarketing", "b2bmarketing", "customerexperience", "productmarketing", "marketingconsultant", "eventmarketing", "MarketingTechnology", "viralmarketing", "marketingmemes", "marketingscience", "brandmanagement", "seo", "bigseo", "techseo", "localseo", "seo_tools", "linkbuilding", "learnseo", "wordpressSEO", "shopifySEO", "ecommerceSEO", "sem", "searchmarketing", "payperclick", "affiliateSEO", "blackhatseo", "whitehatseo", "modernseo", "SEOGurus", "GoogleSearchConsole", "YoutubeSEO", "content_marketing", "copywriting", "askcopywriting", "seo_copywriting", "UXWriting", "blogging", "problogger", "medium", "podcasting", "PodcastingAdvice", "newsletter", "NewsletterGrowth", "emailcopywriting", "B2Bcopywriting", "selfpublish", "freelanceWriters", "brandstorytelling", "Storytelling", "Publishing", "contentstrategy", "socialmedia", "socialmediamarketing", "smm", "InstagramMarketing", "LinkedIn", "Twitter", "TikTokMarketing", "Pinterest", "Snapchat", "InfluencerMarketing", "UserGeneratedContent", "YouTube", "YouTubeMarketing", "RedditAds", "socialmediastats", "communitymanagement", "socialmediatips", "ugcExchange", "socialmediajobs", "OrganicSocial", "ecommerce", "shopify", "woocommerce", "amazonseller", "FBA", "fulfillmentbyamazon", "ebayseller", "Etsy", "dropship", "dropshipping", "printondemand", "merchant", "affiliatemarketing", "AmazonFBA", "ecommercefulfillment", "Crowdfunding", "ShopifyDropship", "EcommerceGrowth", "ShopifyHelp", "ShopifyDev", "advertising", "googleads", "facebookads", "linkedinads", "videoadvertising", "displayads", "nativeads", "adops", "programmatic", "adtech", "admarket", "media_buying", "MarketingPPC", "PPCBeginners", "adwords", "analytics", "googleanalytics", "googleanalytics4", "matomo", "datastudio", "conversionrateopt", "conversionXL", "webanalytics", "cro", "digitalanalytics", "branding", "design", "userexperience", "uxdesign", "graphic_design", "logodesign", "Infographics", "typography", "packagedesign", "brandidentity", "finance", "investing", "financialindependence", "personalfinance", "fintech", "SaaS", "SaaSBusiness", "startupfinance", "businessbanking", "equitycrowdfunding", "sportsmarketing", "restaurantmarketing", "legalmarketing", "realestatemarketing", "dentalmarketing", "vetmarketing", "musicmarketing", "gamedevmarketing", "edtech", "healthcaremarketing", "pharmamarketing", "cleantech", "sustainabilitymarketing", "tourismmarketing", "nonprofitmarketing", "marketingDE", "marketingUK", "asiamarketing", "latamarketing", "afrmarketing"
 ] 
